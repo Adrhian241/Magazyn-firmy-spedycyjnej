444917eb3e55843e4df72e23968b248587379040
Adrian Waligora <adrianwaligora4494@gmail.com>
2025-12-19 15:29:23 +0100

    ustawienie w main.c poczatkowych wartosci oraz stworzenie pamieci dzielonej i semaforow

--- dane.h
+++ dane.h
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
+#include <string.h>
 #include <sys/sem.h>
 #include <sys/msg.h>
 #include <time.h>
---
+++ main.c
@@ -0,0 +1,58 @@
+#include "dane.h"
+void ustaw_semafor(int semid, int numer_semafora, int wartosc){
+    if (semctl(semid,numer_semafora,SETVAL,wartosc)==-1)
+    {
+	    perror("[MAIN] Nie mozna ustawic semafora");
+	    exit(EXIT_FAILURE);
+	else
+	{
+	    printf("[MAIN] semafor %d zostal ustawiony na %d.\n",numer_semafora,wartosc);
+	}
+    }
+int main(){
+    
+    //stworzenie pamieci dzielonej
+    printf("[MAIN] START SYMULACJI MAGAZYNU");
+    int shmid = shmget(KEY_SHM, sizeof(MagazynShared), IPC_CREAT | 0600);
+    if (shmid == -1)
+    {
+	printf("[MAIN] problem z utworzeniem pamieci dzielonej. \n");
+	exit(EXIT_FAILURE);
+    }
+    else prinf("[MAIN] Pamiec dzielona zostala utworzona : %d\n",shmid);
+    
+    //stworzenie semaforow
+    int semid = semget(KEY_SEM, 5, IPC_CREAT | 0600);
+        if (semid==-1)
+        {
+                printf("[MAIN] Nie moglem utworzyc nowych semaforow.\n");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                printf("[MAIN] Semafory zostal utworzone : %d\n",semid);
+        }
+
+    //polaczenie sie z pamieci dzielona
+        MagazynShared *wspolna = (MagazynShared*)shmat(shmid, NULL, 0);
+        if (wspolna == (void*)-1) {
+            perror("[MAIN] Blad shmat");
+            exit(1);
+        }
+    //ustawianie semaforow
+    ustaw_semafor(semid, SEM_MUTEX_TASMA, 1);
+    ustaw_semafor(semid, SEM_MUTEX_CIEZAROWKA, 1);
+    ustaw_semafor(semid, SEM_EMPTY, K);
+    ustaw_semafor(semid, SEM_FULL, 0);
+    ustaw_semafor(semid, SEM_RAMPA, 1);
+
+    //ustawienia poczatkowe tasmy i ciezarowek
+    wspolna->tasma.head = 0;
+    wspolna->tasma.tail = 0;
+    wspolna->tasma.ilosc_paczek = 0;
+    wspolna->tasma.masa_paczek = 0.0;
+    wspolna->ciezarowka.czy_stoi = 0;
+    wspolna->ciezarowka.id_ciezarowki = -1;
+    wspolna->koniec_symulacji = 0;
+    shmdt(wspolna);
+}
